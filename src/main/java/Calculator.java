514dc22f348c186b137ce4d4152992febf00d3fe
Shashidhar Basavaraj Nagaral <shashinagaral123@gmail.com>
2023-03-18 01:21:43 +0530

    handled incorrect inputs in application

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,22 +1,56 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
     public Calculator() {
     }
 
-    public double powerF(double x, double y) {
-        return Math.pow(x, y);
-    }
-
     public double squareRootF(double x) {
+        if (x < 0) {
+            return -1;
+        }
         return Math.sqrt(x);
     }
+
+    public int factorialF(int x) {
+        if(x < 0) {
+            return -1;
+        }
+        int result = 1;
+        for(int i=1; i<=x;i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public double naturalLogarithmF(double x) {
+        double result;
+        if(x == 0) {
+            result = Double.NEGATIVE_INFINITY;
+        } else if (x < 0) {
+            result = -1;
+        } else if (x == Double.POSITIVE_INFINITY) {
+            result = Double.POSITIVE_INFINITY;
+        } else {
+            result = Math.log(x);
+        }
+        return result;
+    }
+
+    public double powerF(double base, double exponent) {
+        if(base < 0 && ((int)exponent != exponent))  {
+            // base is negative and exponent is decimal fraction
+            // i.e. we cannot find nth root of a negative number
+            return Double.NaN;
+        }
+            return Math.pow(base, exponent);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Calculator calculator = new Calculator();
         int choice;
-        double result = 0;
-        double input1, input2;
+        double result;
         do {
             System.out.println("-----------------Calculator-DevOps-----------------");
             System.out.println(
@@ -25,30 +59,47 @@ public class Calculator {
                     "Enter 3 for Natural logarithm (base e).\n" +
                     "Enter 4 for Power function.\n"+
                     "Enter 5 for exit. ");
-            choice = scanner.nextInt();
-
-            switch (choice) {
-                case 1:
-                    input1 = scanner.nextDouble();
-                    result = calculator.squareRootF(input1);
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    input1 = scanner.nextDouble();
-                    input2 = scanner.nextDouble();
-                    result = calculator.powerF(input1, input2);
-                    break;
-                case 5:
-                    scanner.close();
-                    return;
-                default:
-                    System.out.println("Enter correct choice!");
-                    continue;
+            try {
+                choice = scanner.nextInt();
+                switch (choice) {
+                    case 1:
+                        result = calculator.squareRootF(scanner.nextDouble());
+                        if (result == -1) {
+                            System.out.println("Please enter positive number!");
+                        } else {
+                            System.out.println("result: " + result);
+                        }
+                        break;
+                    case 2:
+                        result = calculator.factorialF(scanner.nextInt());
+                        if (result == -1) {
+                            System.out.println("Please enter positive number!");
+                        } else {
+                            System.out.println("result: " + result);
+                        }
+                        break;
+                    case 3:
+                        result = calculator.naturalLogarithmF(scanner.nextDouble());
+                        if (result == -1) {
+                            System.out.println("Please enter positive number!");
+                        } else {
+                            System.out.println("result: " + result);
+                        }
+                        break;
+                    case 4:
+                        result = calculator.powerF(scanner.nextDouble(), scanner.nextDouble());
+                        System.out.println("result: " + result);
+                        break;
+                    case 5:
+                        scanner.close();
+                        return;
+                    default:
+                        System.out.println("Enter correct choice!");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Input type mismatch !");
+                scanner.nextLine();
             }
-            System.out.println("result: " + result);
         } while (true);
     }
 }
